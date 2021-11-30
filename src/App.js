8336fcf7159fb750992431fd6c9e0c94e8dcfaf6
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-11-29 22:07:53 -0800

    created mui theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Home from './pages/Home';
+import theme from './theme';
+
 function App() {
   return (
-    <div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Home />
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
 

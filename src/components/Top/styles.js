22b24412dc20068be74fbb2d213dc81ae425edaa
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-12-03 22:39:07 -0800

    added shadow to image and refactored styles

--- src/components/Top/styles.js
+++ src/components/Top/styles.js
@@ -6,6 +6,14 @@ const useStyles = makeStyles((theme) => ({
         padding: '100px 0 120px 0',
         minHeight: '100vh'
     },
+    infoGrid_Top: {
+        paddingRight: '20px'
+    },
+    welcome_Top: {
+        textTransform: 'uppercase', 
+        fontWeight: 'bold', 
+        paddingBottom: '10px'
+    },
     hiTitle_Top: {
         fontSize: '60px',
         fontWeight: '700',
@@ -16,10 +24,36 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: '700',
         paddingBottom: '10px'
     },
+    info_Top: {
+        paddingBottom: '10px'
+    },
+    sayHiLink_Top: {
+        textDecoration: 'none'
+    },
+    sayHiLinkSpan_Top: {
+        color: theme.palette.text.primary, 
+        letterSpacing: '2px', 
+        textTransform: 'uppercase'
+    },
+    handWave_Top: {
+        marginLeft: '5px',
+        paddingTop: '6px'
+    },
+    signature_Top: {
+        fontFamily: 'Kaushan Script, sans-sefif', 
+        fontWeight: 'bold', 
+        transform: 'skewY(-5deg)', 
+        paddingBottom: '5px', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        paddingRight: '100px', 
+        textShadow: '9px 9px 20px rgb(50,50,50,.5)'
+    },
     imageDiv_Top: {
         height: '700px',
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        boxShadow: theme.shadows[10]
     },
     image_Top: {
         maxHeight: '700px',

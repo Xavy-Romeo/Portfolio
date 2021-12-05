228365cff963c0d52b811e859b360dfb4950fab8
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-12-04 23:00:55 -0800

    added skill set bar and profile image

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -24,6 +24,18 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: '600',
         textShadow: `1px 1px ${theme.palette.text.primary}`
     },
+    bold_About: {
+        fontWeight: '600'
+    },
+    skillBarContainer_About: {
+        width: '100%',
+        height: '100%',
+        borderImage: 'radial-gradient(#f64f59, #12c2e9, #f64f59) 1',
+        border: '3px solid transparent',
+        marginTop: '10px',
+        padding: '20px'
+    }
+
 }));
 
 export default useStyles;

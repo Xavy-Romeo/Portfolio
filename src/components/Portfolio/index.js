c4cac747c46f026d990a9dcf7973f26e88fa5cf7
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-12-07 12:21:51 -0800

    refactored and mapped projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,12 +4,18 @@ import Typography from '@material-ui/core/Typography';
 import { BsArrowUpRight } from 'react-icons/bs';
 
 import useStyles from './styles';
-import testImage from '../../assets/images/xavy1.png';
+import TheXavyShop from '../../assets/images/the-xavy-shop.png';
 import HowRU from '../../assets/images/howru.png';
-import RunBuddy from '../../assets/images/runbuddy.png';
+import RunBuddy from '../../assets/images/run-buddy.png';
 
 const Portfolio = () => {
     const classes = useStyles();
+
+    const projects = [
+        {name: 'The Xavy Shop', type: 'E-commerce Application', languages: 'React, Material-UI, MongoDB, Express, JavaScript, NodeJS, HTML, CSS, GraphQL', image: TheXavyShop},
+        {name: 'HowRU', type: 'Mental Health Application', languages: 'React, Material-UI, MongoDB, Express, JavaScript, AJAX, HTML, CSS, NodeJS', image: HowRU},
+        {name: 'Run Buddy', type: 'Run Buddy Landing Page', languages: 'HTML, CSS', image: RunBuddy}
+    ];
     
     return (
         <section className={`section border-top`} id='portfolio'>
@@ -32,7 +38,66 @@ const Portfolio = () => {
                     </Typography>
                 </Grid>                    
                 <Grid container>
-                    <Grid item xs={4} className={classes.projectGridContainer_Portfolio}>
+                    {projects.map((project, index) => (
+                        <Grid item xs={4} className={classes.projectGridContainer_Portfolio} key={index}>
+                            <Grid 
+                                container 
+                                className={classes.projectGrid_Portfolio}
+                                direction='column' 
+                                justifyContent='center' 
+                                alignItems='center' 
+                            >
+                                <Box className={classes.imgProjectContainer_Portfolio}>
+                                    <img src={project.image} className={classes.imgProject_Portfolio} height='100%' width='100%' alt='' />
+                                    <Box className={classes.imgHoverContainer_Portfolio}>
+                                        <Box className={classes.infoProjectHoverContainer_Portfolio}>
+                                            <Grid 
+                                                container 
+                                                className={classes.infoProjectHover_Portfolio} 
+                                                direction='column' 
+                                                justifyContent='center' 
+                                                alignItems='center' 
+                                            >
+                                                <Typography variant='subtitle2'>
+                                                    {project.name}
+                                                </Typography>
+                                                
+                                                <Typography className={classes.viewProjectHover_Portfolio} variant='caption'>
+                                                    View Project
+                                                </Typography>
+                                            </Grid>
+                                        </Box>
+                                    </Box>
+                                </Box>
+                                <Grid container className={classes.projectInfoContainer_Portfolio} direction='column' justifyContent='space-between'>
+                                    <Grid container direction='column'>
+                                        <Typography className={`${classes.projectAppTitle_Project} text-theme`} variant='body2'>
+                                            {project.type}
+                                        </Typography>
+                                        <Typography className={classes.projectTitle_Project} variant='subtitle1'>
+                                            {project.name}
+                                        </Typography>
+                                        <Typography className={classes.techUsed_Project} variant='caption'>
+                                            Technologies Used: 
+                                        </Typography>
+                                        <Typography className={classes.techUsedInfo_Project} variant='caption'>
+                                            {project.languages}
+                                        </Typography>
+                                    </Grid>
+                                    <Box className={classes.viewProjectContainer_Project}>
+                                        <a href='#' className={classes.viewProjectLink_Project}>
+                                            <Typography className={`${classes.viewProjectLinkTypography_Project} view-project-link`} variant='caption'>
+                                                View Project
+                                                <BsArrowUpRight className={classes.viewProjectArrow_Portfolio} />
+                                            </Typography>
+                                        </a>
+                                    </Box>
+                                    
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    ))}
+                    {/* <Grid item xs={4} className={classes.projectGridContainer_Portfolio}>
                         <Grid 
                             container 
                             className={classes.projectGrid_Portfolio}
@@ -200,7 +265,7 @@ const Portfolio = () => {
                                 </Box>
                             </Grid>
                         </Grid>
-                    </Grid>
+                    </Grid> */}
                 </Grid>
 
                 <Grid container className={classes.seeMoreContainer_Portfolio} justifyContent='center'>

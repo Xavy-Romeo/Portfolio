10057331cfff2df2d1770141b45541f07d5b8dbc
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-01-06 14:46:30 -0800

    added amc project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,27 +1,17 @@
-import TheXavyShop from '../assets/images/the-xavy-shop.png';
+// import TheXavyShop from '../assets/images/the-xavy-shop.png';
 // import HowRU from '../assets/images/howru.png';
 import RunBuddy from '../assets/images/run-buddy.png';
 import Target from '../assets/images/target.png';
 import IntelliRisk from '../assets/images/intellirisk.png';
+import AMC from '../assets/images/amc.png';
 
 const projects = [
-    {
-        name: 'The Xavy Shop', 
-        type: 'E-commerce Application', 
-        languages: 'React, Material-UI, MongoDB, Express, JavaScript, NodeJS, HTML, CSS, GraphQL', 
-        image: TheXavyShop,
-        description: `An E-Commerce application where you can purchase your favorite sporting goods. Uses a react front end, 
-            styled by Material-UI and receives data from a GraphQL api. Global state is managed through react’s context api and 
-            local state is managed using react hooks. Payment processing is powered by the Stipe api.`,
-        site: 'https://the-xavy-shop.herokuapp.com/',
-        github: 'https://github.com/Xavy-Romeo/the-xavy-shop'
-    },
     {
         name: 'IntelliRisk', 
         type: 'IntelliRisk Landing Page', 
         languages: 'HTML, CSS', 
         image: IntelliRisk,
-        description: `Manage your risk all in one app. A landing page for IntelliRisk.`,
+        description: `A landing page for IntelliRisk. Manage your risk all in one app. `,
         site: 'https://xavy-romeo.github.io/intellirisk/',
         github: 'https://github.com/Xavy-Romeo/intellirisk'
     },
@@ -35,11 +25,20 @@ const projects = [
         github: 'https://github.com/Xavy-Romeo/run-buddy'
     },
     {
+        name: 'AMC Email',
+        type: 'Transactional Email',
+        languages: 'HTML, Sass, Foundation, Panini, Inky',
+        image: AMC,
+        description:'A transactional email for Target. Thank you for your order at AMC theatres.',
+        site: 'https://xavy-romeo.github.io/amc-transactional-email/',
+        github: 'https://github.com/Xavy-Romeo/amc-transactional-email'
+    },
+    {
         name: 'Target Email',
         type: 'Promotional Email',
         languages: 'HTML, Sass, Foundation, Panini, Inky',
         image: Target,
-        description:'A promotional email for Target.',
+        description:'A promotional email for Target. Come shop at Target.',
         site: 'https://xavy-romeo.github.io/target-promotional-email/',
         github: 'https://github.com/Xavy-Romeo/target-promotional-email'
     }

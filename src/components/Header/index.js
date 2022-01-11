f815e907554663e9cdddaf5ab28f540cf0a0352d
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-01-11 13:09:29 -0800

    created header animation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import gsap, { Power3 } from 'gsap';
 
 import AppBar from '@material-ui/core/AppBar';
 import Grid from '@material-ui/core/Grid';
@@ -25,25 +26,30 @@ const Header = () => {
     };
 
     useEffect(() => {
-        window.addEventListener('scroll', changeHeader)
+        window.addEventListener('scroll', changeHeader);
     }, []);
+
+    useEffect(() => {
+        gsap.fromTo('.header-title', {x:'-100', opacity: 0}, {x: 0, opacity: 1, duration: 2, delay:1, ease: Power3.easeInOut});
+        gsap.fromTo('.nav-container', {x:'100', opacity: 0}, {x: 0, opacity: 1, duration: 2, delay:1, ease: Power3.easeInOut});
+    }, []); 
 
     return (
         <AppBar className={!headerScroll ? classes.header_Header : `${classes.headerScroll_Header} main-header`}>
             <Container maxWidth='xl'>
                 <Grid container justifyContent='space-between' alignItems='center'> 
-                    <Grid item xs={8} sm={5} lg={3}>
+                    <Grid className='header-title' item xs={8} sm={5} lg={3}>
                         <a href='#home' className={classes.titleLink_Header}>
                             <Typography className={`${classes.headerTitle_Header} text-theme`} variant='h1'>
                                 Xavy Romeo
                             </Typography>
                         </a>
                     </Grid>
-                    <Grid item className={classes.navContainer_Header} xs={2} lg={7}>
+                    <Grid item className={`${classes.navContainer_Header} nav-container`} xs={2} lg={7}>
                         <Nav />
                     </Grid>
                     <Hidden mdDown>
-                        <Grid item className={classes.hireMeBtnContainer_Header} xs={2}>
+                        <Grid item className={`${classes.hireMeBtnContainer_Header} fade-in`} xs={2}>
                             <a href='#contact' className={classes.hireMeBtnLink_Header}>
                                 <Button className={classes.hireMeBtn_Header}>
                                     Hire Me                                

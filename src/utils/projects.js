81b635bfed204a379695e80e7effc58a1b9d2498
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-03-29 22:13:19 -0700

    added email projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -6,6 +6,10 @@ import IntelliRisk from '../assets/images/intellirisk.png';
 import AMC from '../assets/images/amc.png';
 import Caesars from '../assets/images/caesars.png';
 import HGTV from '../assets/images/hgtv.jpg';
+import Bulgari from '../assets/images/bulgari.jpg';
+import Coach from '../assets/images/coach.jpg';
+import Cpk from '../assets/images/cpk.jpg';
+import Relais from '../assets/images/relais.jpg';
 
 const projects = [
     // {
@@ -36,20 +40,11 @@ const projects = [
         github: 'https://github.com/Xavy-Romeo/caesars-landing'
     },
     {
-        name: 'IntelliRisk', 
-        type: 'IntelliRisk Landing Page', 
-        languages: 'HTML, CSS', 
-        image: IntelliRisk,
-        description: `A landing page for IntelliRisk. Manage your risk all in one app. `,
-        site: 'https://xavy-romeo.github.io/intellirisk/',
-        github: 'https://github.com/Xavy-Romeo/intellirisk'
-    },
-    {
         name: 'HGTV Email',
         type: 'Newsletter Email',
-        languages: 'HTML',
+        languages: 'HTML, CSS',
         image: HGTV,
-        description:"A Newsletter email for Target. View this week's HGTV favorites.",
+        description:"A Newsletter email for HGTV. View this week's HGTV favorites.",
         site: 'https://xavy-romeo.github.io/hgtv-newsletter-email/',
         github: 'https://github.com/Xavy-Romeo/hgtv-newsletter-email'
     },
@@ -61,7 +56,52 @@ const projects = [
         description:'A transactional email for AMC. Thank you for your order at AMC theatres.',
         site: 'https://xavy-romeo.github.io/amc-transactional-email/',
         github: 'https://github.com/Xavy-Romeo/amc-transactional-email'
-    }
+    },
+    {
+        name: 'IntelliRisk', 
+        type: 'Landing Page', 
+        languages: 'HTML, CSS', 
+        image: IntelliRisk,
+        description: `A landing page for IntelliRisk. Manage your risk all in one app. `,
+        site: 'https://xavy-romeo.github.io/intellirisk/',
+        github: 'https://github.com/Xavy-Romeo/intellirisk'
+    },
+    {
+        name: 'Bulgari Email',
+        type: 'Newsletter Email',
+        languages: 'HTML, CSS',
+        image: Bulgari,
+        description:"A Newsletter email for Bulgari. Discover our new collection of purses.",
+        site: 'https://xavy-romeo.github.io/bvlgari-newsletter-email/',
+        github: 'https://github.com/Xavy-Romeo/bvlgari-newsletter-email'
+    },
+    {
+        name: 'Coach Email',
+        type: 'Welcome Email',
+        languages: 'HTML, CSS',
+        image: Coach,
+        description:"A Welcome email for Coach. Welcome to the Coach family.",
+        site: 'https://xavy-romeo.github.io/coach-welcome-email/',
+        github: 'https://github.com/Xavy-Romeo/coach-welcome-email'
+    },
+    {
+        name: 'CPK Email',
+        type: 'Promotional Email',
+        languages: 'HTML, CSS',
+        image: Cpk,
+        description:"A Promotional email for CPK. A special deal awaits.",
+        site: 'https://xavy-romeo.github.io/cpk-promo-email/',
+        github: 'https://github.com/Xavy-Romeo/cpk-promo-email'
+    },
+    {
+        name: 'Relais Email',
+        type: 'Newsletter Email',
+        languages: 'HTML, CSS',
+        image: Relais,
+        description:"A Newsletter email for Relais & Chateaux. Read about out spectactular garden series.",
+        site: 'https://xavy-romeo.github.io/relais-newsletter-email/',
+        github: 'https://github.com/Xavy-Romeo/relais-newsletter-email'
+    },
     // {
     //     name: 'Target Email',
     //     type: 'Promotional Email',
